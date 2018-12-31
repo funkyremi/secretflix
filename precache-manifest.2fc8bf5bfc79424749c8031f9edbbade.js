52ff954bdfa49d58b3d770ba23c9a5a515b911cc
Rémi Testa <remi.testa@gmail.com>
2018-12-31 15:41:55 +0100

    Updates

---
+++ precache-manifest.2fc8bf5bfc79424749c8031f9edbbade.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3086fa5794f858042a6",
+    "url": "/secretflix/static/js/runtime~main.b3086fa5.js"
+  },
+  {
+    "revision": "c573afbdb2dc779eaeb4",
+    "url": "/secretflix/static/js/main.c573afbd.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/secretflix/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "c573afbdb2dc779eaeb4",
+    "url": "/secretflix/static/css/main.93a9e6e4.chunk.css"
+  },
+  {
+    "revision": "176b8a32971ff4fc93c4baf0c83b1915",
+    "url": "/secretflix/index.html"
+  }
+];
